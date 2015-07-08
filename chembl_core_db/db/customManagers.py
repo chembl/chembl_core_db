63dd74296798b346e5367ab33079187d1e6525d6
Michal Nowotka <mnowotka@ebi.ac.uk>
2015-07-08 11:59:43 +0100

    alility to specify fingrprints with better defaults

--- chembl_core_db/db/customManagers.py
+++ chembl_core_db/db/customManagers.py
@@ -3,6 +3,7 @@ __author__ = 'mnowotka'
 from django.db import models
 from django.db import connections
 from django.db.models.query import QuerySet
+from django.conf import settings
 
 #-----------------------------------------------------------------------------------------------------------------------
 
@@ -11,9 +12,9 @@ class CompoundMolsMixin(object):
     def get_column(self, name):
         return filter(lambda x: x.name == name, self.model._meta.fields)[0].db_column or name
 
-    def similar_to(self, structure, similarity_index):
-        ctab_column = self.get_column('ctab')
-        molregno_column = self.get_column('molecule')
+    def similar_to(self, structure, similarity_index, **kwargs):
+        ctab_column = kwargs.get('ctab_column', self.get_column('ctab'))
+        molregno_column = kwargs.get('molecule_column', self.get_column('molecule'))
         try:
             sim = int(similarity_index)
         except ValueError:
@@ -28,14 +29,23 @@ class CompoundMolsMixin(object):
                 where=["molsim (" + ctab_column + ", %s, 'normal') BETWEEN %s AND '100'"],
                 params=('smiles:' + structure, similarity_index))
         if connection.vendor == 'postgresql':
+            fingerprints_table = kwargs.get('fingerprints_table', getattr(settings, 'FINGERPRINTS_TABLE', 'fps_rdkit'))
+            fingerprints_pk = kwargs.get('fingerprints_pk', getattr(settings, 'FINGERPRINTS_PK', 'molregno'))
+            similarity_type = kwargs.get('similarity_type', getattr(settings, 'SIMILARITY_TYPE', 'tanimoto_sml'))
+            fingerprint_type = kwargs.get('fingerprint_type', getattr(settings, 'FINGERPRINT_TYPE', 'morganbv_fp'))
+            fingerprint_column = kwargs.get('fingerprint_column', getattr(settings, 'FINGERPRINT_COLUMN', 'mfp2'))
             cursor = connection.cursor()
             cursor.execute("select 'c1ccccc1O'::mol;") # dirty hack but what can I do...
             cursor.execute('set rdkit.tanimoto_threshold=%s;', (sim / 100.0,))
-            ret = self.extra(select={'similarity': "tanimoto_sml(torsionbv_fp(%s), torsionbv)"},
+            ret = self.extra(
+                select={'similarity': similarity_type + "(" + fingerprint_type + "(%s), " + fingerprint_column + ")"},
                 select_params=(structure,),
-                where=["fps_rdkit.molregno = " + self.model._meta.db_table + "." + molregno_column +
-                       " and torsionbv_fp(%s) %% torsionbv and tanimoto_sml(torsionbv_fp(%s), torsionbv) between %s and 1.0"],
-                tables=['fps_rdkit'],
+                where=[
+                    fingerprints_table + "." + fingerprints_pk + " = " + self.model._meta.db_table + "." +
+                    molregno_column + " and " + fingerprint_type + "(%s) %% " + fingerprint_column + " and " +
+                    similarity_type + "(" + fingerprint_type + "(%s), " + fingerprint_column + ") between %s and 1.0"
+                ],
+                tables=[fingerprints_table],
                 order_by=['-similarity'],
                 params=[structure, structure, (sim / 100.0)])
             cursor.execute('set rdkit.tanimoto_threshold=0.5;')
@@ -57,7 +67,8 @@ class CompoundMolsMixin(object):
         ctab_column = self.get_column('ctab')
         connection = connections[self._db or 'default']
         if connection.vendor == 'oracle':
-            return self.extra(where=["(flexmatch(" + ctab_column + ",%s,'ignore=all')=1)"], params=('smiles:' + structure,))
+            return self.extra(
+                where=["(flexmatch(" + ctab_column + ",%s,'ignore=all')=1)"], params=('smiles:' + structure,))
         if connection.vendor == 'postgresql':
             try:
                 from rdkit.rdBase import rdkitVersion
@@ -65,7 +76,8 @@ class CompoundMolsMixin(object):
                 rdkitVersion = 0
             if rdkitVersion != '2015.03.1':
                 return self.extra(where=[ctab_column + "@=%s"], params=(structure,))
-            return self.extra(where=[ctab_column + "@>%s" + " AND " + ctab_column + "<@%s"], params=(structure, structure))
+            return self.extra(
+                where=[ctab_column + "@>%s" + " AND " + ctab_column + "<@%s"], params=(structure, structure))
         else:
             raise NotImplementedError
 
